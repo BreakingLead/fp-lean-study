a39507c52ee656b809233a10bfd2666920381ebd
BreakingLead <breakingleadqwq@gmail.com>
2026-07-15 20:20:09 +0800

    Implement LogMonad with isEven, sumAndFindEvens, and inorderSum functions

--- Main.lean
+++ Main.lean
@@ -46,3 +46,82 @@ def firstThird'2 (xs : List α) : Option (α × α) :=
     some (first, third)
 
 #eval firstThird'2 [6,7,8,9]
+
+
+
+namespace LogMonad
+
+def isEven : Int → Bool :=
+  fun i => (i % 2) == 0
+
+-- def sumAndFindEvens : List Int → List Int × Int
+--   | [] => ([], 0)
+--   | x :: xs =>
+--     let (evens, sum) := sumAndFindEvens xs
+--     (if isEven x then x :: evens else evens, x + sum)
+
+def sumAndFindEvens : List Int → List Int × Int
+  | [] => ([], 0)
+  | x :: xs =>
+    let nowLog := if isEven x then [x] else []
+    let nowSum := x
+    let {fst := nextLog, snd := nextSum} := (sumAndFindEvens xs)
+
+    (nowLog ++ nextLog, nowSum + nextSum)
+
+
+
+#eval sumAndFindEvens [1,2,3,4,5,6,7,8,9]
+
+structure WithLog (logged : Type) (α : Type) where
+  log : List logged
+  val : α
+
+def andThen (result: WithLog α β) (next: β → WithLog α γ) : WithLog α γ :=
+  let new := next result.val
+  {
+    log := result.log ++ new.log
+    val := new.val
+  }
+
+def ok (x : β) : WithLog α β := {log := [], val := x}
+def save (x : α) : WithLog α Unit := {log := [x], val := ()}
+
+-- def andThen (result: WithLog α β) (next: β → WithLog α γ) : WithLog α γ :=
+--   let {log := thisLog, val := thisVal} := result;
+--   let {log := nextLog, val := nextVal} := next thisVal;
+--   {
+--     log := thisLog ++ nextLog
+--     val := nextVal
+--   }
+
+infixl:55 " ~~> " => andThen
+
+
+def sumAndFindEvens2 : List Int → WithLog Int Int
+  | [] => ok 0
+  | x :: xs =>
+    (if isEven x then save x else ok ()) ~~> fun _ =>
+    (sumAndFindEvens2 xs) ~~> fun value =>
+    ok (x + value)
+
+#eval sumAndFindEvens2 [1,2,3,4,5,6,7,8,9]
+
+inductive BinTree (α : Type) where
+  | leaf : BinTree α
+  | branch : BinTree α -> α -> BinTree α -> BinTree α
+
+-- 遍历并求和
+def inorderSum : BinTree Int → WithLog Int Int
+  | BinTree.leaf => ok 0
+  | BinTree.branch l x r =>
+    (inorderSum l) ~~> fun lsum =>
+    save x ~~> fun _ =>
+    (inorderSum r) ~~> fun rsum =>
+    ok (lsum+x+rsum)
+
+def t1: BinTree Int := BinTree.branch (BinTree.branch BinTree.leaf 1 BinTree.leaf) 8 (BinTree.branch BinTree.leaf 3 BinTree.leaf)
+
+#eval inorderSum t1
+
+end LogMonad
