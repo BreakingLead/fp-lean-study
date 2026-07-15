aa4ee8cf657bb988668bfd41c804da1a0daae77b
BreakingLead <breakingleadqwq@gmail.com>
2026-07-15 18:01:43 +0800

    update

--- Main.lean
+++ Main.lean
@@ -2,3 +2,18 @@ import FpLeanStudy
 
 def main : IO Unit :=
   IO.println s!"Hello, {hello}!"
+
+def a := #["lol", "cat"]
+
+#eval a[3]?
+
+class MyMonoid (α : Type) where
+  mempty : α
+  mappend : α → α → α
+
+instance : MyMonoid Nat where
+  mempty := 0
+  mappend := fun x y => x + y
+
+
+#eval MyMonoid.mappend 3 4
